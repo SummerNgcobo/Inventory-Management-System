7ce77ed155bb6061e395cf71b812d78ca4377fa7
Summer <summerngcobo02@gmail.com>
2025-03-25 12:22:37 +0200

    updates

--- Inventory.Model/Currency.cs
+++ Inventory.Model/Currency.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Inventory.Model
 {
-    internal class Currency
+    public class Currency
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+
+
     }
 }
--- Inventory.Repository/ApplicationDbContext.cs
+++ Inventory.Repository/ApplicationDbContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Inventory.Repository
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options);
     }
 }
